56107f6dcc44738e02116d7fab012441451466c0
unknown <richardbacallao@knights.ucf.edu>
2021-10-03 13:49:01 -0400

    Animation commit

--- Assets/Scripts/SoundScript.cs
+++ Assets/Scripts/SoundScript.cs
@@ -11,11 +11,13 @@ public class SoundScript : MonoBehaviour
 
     public AudioSource musicSource;
 
+    Animator anim;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -25,23 +27,27 @@ public class SoundScript : MonoBehaviour
         {
             musicSource.clip = musicClipOne;
             musicSource.Play();
+            anim.SetInteger("State", 1);
         }
 
         if (Input.GetKeyUp(KeyCode.W))
         {
             musicSource.Stop();
+            anim.SetInteger("State", 0);
         }
 
         if (Input.GetKeyDown(KeyCode.R))
         {
             musicSource.clip = musicClipTwo;
             musicSource.Play();
+            anim.SetInteger("State", 2);
 
         }
         
         if (Input.GetKeyUp(KeyCode.R))
         {
             musicSource.Stop();
+            anim.SetInteger("State", 0);
         }
 
         if (Input.GetKeyDown(KeyCode.L))
